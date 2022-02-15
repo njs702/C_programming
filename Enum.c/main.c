891423a2f9ee1b073a5fc23a74747a589336e71f
junseok <njs702@ajou.ac.kr>
2022-02-15 11:54:08 +0900

    enum 자료형 활용

---
+++ Enum.c/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+enum origin { EAST, WEST, SOUTH, NORTH };
+
+void move(enum origin to) {
+    printf("%d쪽으로 이동합니다.\n", to);
+}
+
+void moveChar(enum origin to) {
+    char* moveTo[4] = { "EAST","WEST","SOUTH","NORTH" };
+    printf("%s쪽으로 이동합니다.\n", moveTo[to]);
+}
+
+int main()
+{
+
+    enum origin mark = WEST;
+    enum origin head = SOUTH;
+    move(EAST);
+    move(mark);
+    move(head);
+    move(NORTH);
+
+    moveChar(EAST);
+    return 0;
+}
