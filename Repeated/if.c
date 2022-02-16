e6ba7cd49cc8c1cb602a81ef23622405ff191045
junseok <njs702@ajou.ac.kr>
2022-02-16 10:44:09 +0900

    using header file practice

---
+++ Repeated/if.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int if_() {
+	int input;
+
+	printf("### 2 + 3 판별 프로그램 ###\n");
+	printf("2 + 3을 입력하세요 : ");
+	scanf_s("%d", &input);
+
+	if (input == 5) {
+		printf("정답입니다.\n");
+		puts("Congratulation!\n");
+	}
+	else if (input == 6) {
+		printf("곱셈이 아니라 덧셈입니다.\n");
+	}
+	else {
+		printf("틀렸습니다.\n");
+	}
+	printf("### 2 + 3 판별 종료 ###\n");
+}
